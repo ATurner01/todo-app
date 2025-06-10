50e82f2453083b5351babd0e78f1cc4e72967f84
Adam Turner <aturner5278@gmail.com>
2025-06-10 03:11:38 +0100

    Setup base database functionality

--- src/app/create.js
+++ src/app/create.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { Task } from './todo';
 
 function CreateTaskForm({ title, description, onSubmit, onTitleChange,       onDescriptionChange }) {
     return (
