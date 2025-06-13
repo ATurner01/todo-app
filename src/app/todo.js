7658b7219d0b2456292f05021b22373297dcd687
Adam Turner <aturner5278@gmail.com>
2025-06-13 23:03:13 +0100

    Fix complete button functionality

--- src/app/todo.js
+++ src/app/todo.js
@@ -1,41 +1,48 @@
 'use client';
 
-import { setCompleted } from './actions';
+import { setCompleted } from "./actions";
+import { useRef } from "react";
 
-export function Task({ title, description, onCompleteClick }) {
+function Task({ id, title, description }) {
+
+    const formRef = useRef(null);
+
+    async function handleClick(formData) {
+        const res = await setCompleted(formData);
+
+        if (res.success) {
+            console.log("Task marked as complete.");
+        } else {
+            console.error("Unable to mark task as complete." || res.message)
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center border p-4 rounded px-16">
             <div className="border-b mb-4">
                 <h1 className="text-4xl font-bold mb-6">{title}</h1>
             </div>
+
             <p className="text">{description}</p>
-            <button className="bg-blue-500 text-white p-2 rounded mt-4" onClick={onCompleteClick}>Mark as Completed</button>
+
+            <form ref={formRef} action={handleClick} >
+                <input type="hidden" name="id" value={id} />
+                <button type="submit" className="bg-blue-500 text-white p-2 rounded mt-4">Mark as Complete</button>
+            </form>
         </div>
     );
 }
 
 export function TaskList( { tasks } ) {
-    
-    async function handleClick(taskId) {
-        const data = new FormData();
-        data.append('id', taskId);
-
-        const response = await setCompleted(data);
-
-        if (response.success) {
-            console.log(response.message);
-        } else {
-            console.error('Error:', response.message);
-        }
-    }
+    const taskList = JSON.parse(tasks)
 
     return (
         <div className="flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold mb-6 underline">Task List</h1>
             <ul className="list-none">
-                {tasks.map((task, _) => (
+                {taskList.map(task => (
                     <li key={task.id} className="mb-10">
-                        <Task title={task.title} description={task.description} onCompleteClick={() => handleClick(task.id)} />
+                        <Task id={task.id} title={task.title} description={task.description}  />
                     </li>
                 ))}
             </ul>
